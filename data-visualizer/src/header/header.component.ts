20194b4dbb2667d43c54ea76c020650a48d9d631
Utkarsh Jain <xutkarshjain@gmail.com>
2025-06-25 00:23:05 +0530

    Feat: Add header with greeting and buttons

---
+++ data-visualizer/src/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  userName: string = 'Olivia';
+  greeting: string = 'Welcome back';
+}
